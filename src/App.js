9a2e4820dc16932a5eff3b5d9bc6ea2346ae17bd
Mahi-hub-spec <m.mmahi940@gmail.com>
2025-11-11 20:41:06 +0500

    if else assignment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { My_named_Component,Another_named_Component } from "./practice/practice_import";
 import default_Component from "./practice/practice_export";
-const App = () => {
-return( 
-  <div>
-    <My_named_Component/>
-    <Another_named_Component/>
-    <default_Component/>
-  </div>
-  
-);
+const Welcome = (props) => {
+  return <div>{props.children}</div>;
+};
 
+const App = () => {
+  return (
+    <Welcome>
+      <h1>Hello, World!</h1>
+      <p>Welcome to React.</p>
+    </Welcome>
+  );
 };  
 export default App;
--- src/practice/practice_import.js
+++ src/practice/practice_import.js
@@ -1,7 +1,13 @@
 export const My_named_Component = () => {
-    return <h1>We will be learning Export and Import here</h1>;
+    return 
+        <h1>
+            We will be learning Export and Import here
+        </h1>;
 };
 
 export const Another_named_Component = () => {
-    return <p2>We got a cheatsheet in node which includes git status, git add, git commit, git push, git checkout, git switch and npm start  </p2>;
+    return
+    <p2>
+        We got a cheatsheet in node which includes git status, git add, git commit, git push, git checkout, git switch and npm start  
+        </p2>;
 };
