6f5384f305ea4b6c37dfb8d4433768ed349a9a2a
Mahi-hub-spec <m.mmahi940@gmail.com>
2025-11-11 20:58:49 +0500

    Learning if else in react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { My_named_Component,Another_named_Component } from "./practice/practice_import";
-import default_Component from "./practice/practice_export";
-const Welcome = (props) => {
-  return <div>{props.children}</div>;
-};
-
-const App = () => {
-  return (
-    <Welcome>
-      <h1>Hello, World!</h1>
-      <p>Welcome to React.</p>
-    </Welcome>
-  );
-};  
-export default App;
+let dayNumber = prompt("Enter a number between 1 and 7:");
+// Promt() is used for taking a value from user 
+if (dayNumber == 1) {
+  alert("It's Monday"); //alert is used for showing a message on a screen 
+} else if (dayNumber == 2) {
+  alert("It's Tuesday");
+} else if (dayNumber == 3) {
+  alert("It's Wednesday");
+} else if (dayNumber == 4) {
+  alert("It's Thursday");
+} else if (dayNumber == 5) {
+  alert("It's Friday");
+} else if (dayNumber == 6) {
+  alert("It's Saturday");
+} else if (dayNumber == 7) {
+  alert("It's Sunday");
+} else {
+  alert("This Day Come on coming week, Please enter a number between 1 and 7.");
+}
