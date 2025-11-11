ebfd4855be6a06da1b144eff82f520b1579c399c
Mahi-hub-spec <m.mmahi940@gmail.com>
2025-11-11 13:26:05 +0500

    lets learn react

--- src/practice/practice_export.js
+++ src/practice/practice_export.js
@@ -3,7 +3,7 @@ import React from "react";
 const default_Component = () => {
     return
      
-     <p5>We got a cheatsheet in node which includes git status, git add, git commit, git push, git checkout, git switch and npm start  </p5>;
+     <p2>We got a cheatsheet in node which includes git status, git add, git commit, git push, git checkout, git switch and npm start  </p2>;
 };
 
 export default default_Component;
